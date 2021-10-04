2bb69fd7c4c4b705d8671276321478c56768e9ff
Shafiq Suhag <shafiqsuhag@gmail.com>
2021-10-04 17:48:15 +0600

    about us page update

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -3,24 +3,74 @@ import HeroSection from '../../sections/HeroSection/HeroSection';
 
 const About = () => {
     return (
-        <div>
+        <div >
             {/* hero section  */}
             <HeroSection title="ABOUT US" bgImg="./images/contact-cover.png"></HeroSection>
 
             {/* about us page info  */}
-            <div>
-                <div>
-                    imgage
+            <div className="grid grid-cols-2 mt-20 mx-10 ">
+                <div className="flex items-center justify-center ">
+                    <img src="./images/about-us.webp" alt="about us " className=" rounded shadow-lg" />
                 </div>
-                <div>
-                Welcome to Edule.
-You can join with Edule and upgrade your skill for your bright future.
-Lorem Ipsum has been the industr’s standard dummy text ever since unknown printer took galley type and scmbled make type specimen book. It has survived not only five centuries.
+                <div className="flex flex-col items-start justify-center pr-28">
+                    <h1 className="text-xl text-green-500">
+                        Welcome to Pro Learner.
+                    </h1>
+                    <p className="text-4xl my-5 font-medium	">
+                        You can join with PRO LEARNER and upgrade your skill for your bright future.
+
+                    </p>
+                    <p className="my-5">
+                        Whether you want to learn or to share what you know, you’ve come to the right place. As a global destination for online learning, we connect people through knowledge.
+                    </p>
+                    <button class="btn btn-outline btn-accent">BROWSE COURSES</button>
+
+
                 </div>
             </div>
+
+            {/* card info  */}
+
+            {/* card item  */}
+            <div className="grid grid-cols-3 gap-4 my-20 mx-10">
+                {/* card item  */}
+                <div className="bg-indigo-600 p-10 text-white hover:bg-indigo-500">
+                    <h1 className="text-2xl my-2">
+                        Work with us
+                    </h1>
+                    <p>
+                        At Udemy, we’re all learners and instructors. We live out our values every day to create a culture that is diverse, inclusive, and committed to helping employees thrive.
+                    </p>
+                </div>
+                {/* card item  */}
+                {/* card item  */}
+                <div className="bg-yellow-600 p-10 text-white hover:bg-yellow-500">
+                    <h1 className="text-2xl my-2">
+                        See our research
+                    </h1>
+                    <p>
+                        We’re committed to changing the future of learning for the better. Dig into our original research to learn about the forces that are shaping the modern workplace.
+                    </p>
+                </div>
+                {/* card item  */}
+                {/* card item  */}
+                <div className="bg-green-600 p-10 text-white hover:bg-green-500">
+                    <h1 className="text-2xl my-2">
+                        Read our blog
+                    </h1>
+                    <p>
+                        Want to know what we’ve been up to lately? Check out the Udemy blog to get the scoop on the latest news, ideas and projects, and more.
+                    </p>
+                </div>
+                {/* card item  */}
+            </div>
+            {/* card info  */}
+
             {/* about us page info  */}
         </div>
     );
 };
+
+
 
 export default About;
